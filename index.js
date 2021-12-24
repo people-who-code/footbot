ed01703489a6d5f107fe0f26c5799e5c296efd9d
Abukari Einus <blackmartyr00@gmail.com>
2021-12-24 17:19:36 +0000

    Hello Tweet

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Let go");
+
+const Twit = require('twit');
+require('dotenv').config()
+
+const T = new Twit({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_KEY_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  });
+  
+  T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+    console.log(data)
+  })
+   

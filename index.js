bb9f7944f3a491ce4594f7180c7e98893ab7ae4a
Abukari Einus <blackmartyr00@gmail.com>
2021-12-25 22:32:21 +0000

    added retweet from popular football accounts

--- index.js
+++ index.js
@@ -9,19 +9,36 @@ const T = new Twit({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
   });
   
-  // T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-  //   console.log(data)
-  // })
+
 
 // use this to log errors from requests
 function responseCallback (err, data, response) {
   console.log(err);
  }
- 
- // stream tweets from real madrid official account 
-  const stream = T.stream('statuses/filter', {track: '@realmadriden'});
+ var users = [
+  '26809005', // @goal
+  '18091004', // @ESPNFC
+  '2371041', // @football
+  '443668407', // @SkyFootball
+  '140070953', // @FIFAcom
+  '627673190', // @ChampionsLeague
+  '11096152', // @101greatgoals
+  '44606764', // @OptaJoe
+  ' 87036741', // @Zonal_marking
+  '176754253', // @BeardedGenius
+  '11096152' // @101greatgoals
 
-  stream.on('tweet', tweet => {
-    T.post('statuses/retweet/:id', {id: tweet.id_str}, responseCallback);
-    });
+  
+  
+  ];
+  //'1059736783376842753' //@me
+  
+ var stream = T.stream('statuses/filter', { follow: users });
+
+  stream.on('tweet', function (tweet) {
+      if(!users.includes(tweet.user.id_str)) {return;}
+      T.post('statuses/retweet/:id', {id: tweet.id_str}, responseCallback);
+      console.log(tweet);
+      });
+ 
    

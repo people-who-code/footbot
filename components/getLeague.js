9f65a56da24fade167644e9398c948bf2eee6482
EurikaProg201 <wworgba@gmail.com>
2021-12-29 10:19:52 +0000

    new

--- components/getLeague.js
+++ components/getLeague.js
@@ -1,6 +1,8 @@
 var axios = require('axios');
 var dotenv = require('dotenv');
 dotenv.config();
+module.exports.getLeague = (season, searchQuery, isCurrent) => {
+    var result = "";
     var config = {
         method: 'get',
         url: 'https://v3.football.api-sports.io/leagues',
@@ -8,13 +10,15 @@ dotenv.config();
             'x-rapidapi-key': process.env.FOOT_BALL_API_KEY,
             'x-rapidapi-host': 'v3.football.api-sports.io'
         },
-        params: { season:2021, name:"Druga HNL",current:"true" }
+        params: { season: season, search: searchQuery, current: isCurrent }
     };
 
     axios(config)
         .then(function (response) {
-            console.log(JSON.stringify(response.data));
+            result = JSON.stringify(response.data);
         })
         .catch(function (error) {
             console.log(error);
         });
+    return result
+}
--- components/leagueReply.js
+++ components/leagueReply.js
@@ -1,18 +1,22 @@
 const T = require('../config');
+const { getLeague } = require('./getLeague');
 
-const leagueReply = () =>
-{
-    var replystream = T.stream('statuses/filter', { track: '@footb0t league' });
+const leagueReply = () => {
+  var replystream = T.stream('statuses/filter', { track: '@footb0t league' });
 
-replystream.on('tweet', function (tweet) {
+  replystream.on('tweet', function (tweet) {
+    const season = tweet.match(/\b\d{4}\b/g)[0];
+    const isCurrent = new Date().getFullYear() == season ? true : false;
+    const seachQuery = tweet.replace(/\b\d{4}\b/g, "");
+    // T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
+    // console.log(tweet.id_str);
+    var output = getLeague(season, seachQuery, isCurrent);
+    var replyForQueryTweet = `Name:${output.response[0].league.name}\n Country:${output.response[0].country['name']}\n Seasons:${output.response[0].seasons['start']}-${output.response[0].seasons['end']}`
+    T.post('statuses/update', { status: "wah goan mi gee? mi cyaan provide such info right now but soon!! Trust mi chargie", in_reply_to_status_id: tweet.id_str }, function (err, data, response) {
+      console.log(response)
+    });
 
-      // T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
-      console.log(tweet.id_str);
-       T.post('statuses/update', { status: "wah goan mi gee? mi cyaan provide such info right now but soon!! Trust mi chargie", in_reply_to_status_id:tweet.id_str }, function (err, data, response) {
-        console.log(response)
-      });
-
-});
+  });
 }
 
 module.exports = leagueReply;

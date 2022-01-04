e1a64e4429de6d617521db794029275bd9929c99
Abukari Einus <blackmartyr00@gmail.com>
2022-01-04 10:44:13 +0000

    fixed some bugs

--- components/getLeague.js
+++ components/getLeague.js
@@ -1,24 +1,29 @@
 var axios = require('axios');
-var dotenv = require('dotenv');
-dotenv.config();
-module.exports.getLeague = (season, searchQuery, isCurrent) => {
+require('dotenv').config()
+
+const getLeague = async(season, searchQuery, isCurrent) => {
     var result = "";
     var config = {
         method: 'get',
         url: 'https://v3.football.api-sports.io/leagues',
         headers: {
-            'x-rapidapi-key': process.env.FOOT_BALL_API_KEY,
+            'x-rapidapi-key': process.env.FOOTBALL_API_KEY,
             'x-rapidapi-host': 'v3.football.api-sports.io'
         },
-        params: { season: season, search: searchQuery, current: isCurrent }
+        params: {search: searchQuery,current:true}
     };
 
-    axios(config)
+    await axios(config)
         .then(function (response) {
-            result = JSON.stringify(response.data);
+         
+         result = response.data;
         })
         .catch(function (error) {
             console.log(error);
         });
-    return result
+
+        return result;
+    
 }
+
+module.exports = getLeague;
--- components/leagueReply.js
+++ components/leagueReply.js
@@ -1,20 +1,36 @@
 const T = require('../config');
-const { getLeague } = require('./getLeague');
+const getLeague = require('./getLeague');
 
-const leagueReply = () => {
+const leagueReply = async() => {
   var replystream = T.stream('statuses/filter', { track: '@footb0t' });
-
+  var output = await getLeague('UEFA 2021');
+  //console.log(output.response[0].league.name);
+  for(var x =0;x<800;x++)
+  {
+    var j = output.response[x].league.name.includes('World Cup');
+    if(j)
+    {
+      console.log(output.response[x].league.name)
+    }
+  }
   replystream.on('tweet', function (tweet) {
     const season = tweet.text.match(/\b\d{4}\b/g)[0];
     const isCurrent = new Date().getFullYear() == season ? true : false;
-    const seachQuery = tweet.replace(/\b\d{4}\b/g, "");
-    // T.post('statuses/retweet/:id', { id: tweet.id_str }, responseCallback);
-    // console.log(tweet.id_str);
-    var output = getLeague(season, seachQuery, isCurrent);
-    var replyForQueryTweet = `Name:${output.response[0].league.name}\n Country:${output.response[0].country['name']}\n Seasons:${output.response[0].seasons['start']}-${output.response[0].seasons['end']}`
-    T.post('statuses/update', { status: replyForQueryTweet, in_reply_to_status_id: tweet.id_str }, function (err, data, response) {
-      console.log(response)
-    });
+    const searchQuery = tweet.text.replace(/\b\d{4}\b/g, "");
+    //var output = getLeague(season, searchQuery, isCurrent);
+   
+
+//     T.post('statuses/retweet/:id', { id: tweet.id_str }, (err, data, response) => {
+//     console.log(err);
+//   }
+// );
+    //console.log(tweet.id_str);
+    
+    //var replyForQueryTweet = `Name:${output.response[0].league.name}\n Country:${output.response[0].country['name']}\n Seasons:${output.response[0].seasons['start']}-${output.response[0].seasons['end']}`
+//     T.post('statuses/update', { status: replyForQueryTweet, in_reply_to_status_id: tweet.id_str }, function (err, data, response) {
+//      console.log(response)
+//    });
+
 
   });
 }
